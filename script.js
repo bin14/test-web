0189a189ea9ae04c8c84de12ec50421223721b0a
bin14 <67566670+bin14@users.noreply.github.com>
2022-05-02 17:03:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -162,10 +162,11 @@ document.ontouchstart = evt => birthday.onClick(evt);
 // function([string1, string2],target id,[color1,color2])
 consoleText(
   [
+    "To Peter",
     "Sending you and your family my best wishes on the auspicious occasion of Eid-Ul-Fitr.",
     "Praying for everyone’s good health and well-being inshallah.",
     "With all our duas.",
-    "Smith Family.",
+    "From the Smith Family.",
   ],
   "text",
   ["tomato", "rebeccapurple", "lightblue"]
